f8628290c08696e3ab7c74436e000f795ce855a8
Justin Jones <jus.jones@yahoo.com>
2022-07-04 18:59:51 -0400

    commit tree 22c7ad33f65c99f358e4d2f5b9b8dd72b1380d64

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Your code here
+
+class Cat {
+
+  constructor(name, sex){
+
+    this.name = name
+    this.sex = sex
+  }
+
+  speak(){
+
+    return this.name + " says meow!"
+  }
+
+
+
+}
+
+let cat = new Cat("Sasha")
+
+class Dog{
+
+  constructor(name, sex){
+
+    this.name = name
+    this.sex = sex
+  }
+
+  speak(){
+
+    return this.name + " says woof!"
+  }
+
+}
+
+let dog = new Dog("Roy")
+
+
+
+class Bird {
+
+  constructor(name, sex){
+
+
+    this.name = name
+    this.sex = sex
+  }
+
+  speak(){
+
+    return 'It\'s me!'+ this.name + 'the parrot!'
+  }
+
+}
+
+
+let bird = new Bird('Pablo');
+let bird2 = new Bird('Squawk!')
